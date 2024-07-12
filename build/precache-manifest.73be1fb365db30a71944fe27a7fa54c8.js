5a60287530c8d0ff74d43594ae934f02320fd2b1
YASH GAUTAM <80680021+yashgautam06@users.noreply.github.com>
2024-07-12 14:01:29 +0530

    Add files via upload

---
+++ build/precache-manifest.73be1fb365db30a71944fe27a7fa54c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3abcd023a70450cc9725623d8258bda",
+    "url": "/NewsWebsite/index.html"
+  },
+  {
+    "revision": "6d36c43e5ba720da4f55",
+    "url": "/NewsWebsite/static/css/main.c79ba2c5.chunk.css"
+  },
+  {
+    "revision": "a336aeee0956617dfa53",
+    "url": "/NewsWebsite/static/js/2.f442603d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NewsWebsite/static/js/2.f442603d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d36c43e5ba720da4f55",
+    "url": "/NewsWebsite/static/js/main.4ea12eca.chunk.js"
+  },
+  {
+    "revision": "ace7d5ae8c2c91ab6774",
+    "url": "/NewsWebsite/static/js/runtime-main.9b44a0df.js"
+  }
+]);
